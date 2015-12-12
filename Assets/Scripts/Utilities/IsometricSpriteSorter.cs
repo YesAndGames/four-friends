cfc85b25dd66c1e18986d3c6c748af476263f830
Weslo <wesley.rockholz@gmail.com>
2015-12-12 12:01:59 -0500

    sprites sort isometrically

---
+++ Assets/Scripts/Utilities/IsometricSpriteSorter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class IsometricSpriteSorter : MonoBehaviour {
+	
+	/// <summary>
+	/// Update this instance; set the sorting order based on height.
+	/// </summary>
+	void Update () {
+		this.GetComponent<Renderer> ().sortingOrder = (int)(transform.position.y * -10);
+	}
+}
