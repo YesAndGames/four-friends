77534c76316d309f5c204cbefb9a0c4555901c42
Weslo <wesley.rockholz@gmail.com>
2015-12-12 11:04:20 -0500

    set up a basic game scene

---
+++ Assets/Scripts/Friend.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Controls a single friend in the party.
+/// </summary>
+public class Friend : MonoBehaviour {
+
+	/// <summary>
+	/// Each of this Friend's directional sprites.
+	/// </summary>
+	[SerializeField] private Sprite northSprite, eastSprite, southSprite, westSprite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Party.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Manages controls that affect the whole party.
+/// </summary>
+public class Party : MonoBehaviour {
+
+	/// <summary>
+	/// References to each of the party members.
+	/// </summary>
+	[SerializeField] private Friend northFriend, eastFriend, southFriend, westFriend;
+
+	/// <summary>
+	/// The distance that the party spreads out.
+	/// </summary>
+	[SerializeField] private float partySpread = 1f;
+
+	/// <summary>
+	/// The speed that the party rotates.
+	/// </summary>
+	[SerializeField] private float rotateSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
