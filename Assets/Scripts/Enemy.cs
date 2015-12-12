971b0f5b8861c898235c92d5d234ea05425a3ed0
Weslo <wesley.rockholz@gmail.com>
2015-12-12 13:31:41 -0500

    implement basic ai

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,15 +6,30 @@
 public class Enemy : MonoBehaviour {
 
 	/// <summary>
+	/// The movement speed of this enemy.
+	/// </summary>
+	[SerializeField] private float movementSpeed = 1f;
+
+	/// <summary>
+	/// This enemy's target Friend.
+	/// </summary>
+	private Friend target;
+
+	/// <summary>
 	/// Initialize this component.
 	/// </summary>
 	void Start () {
-		
+		ReevaluateBehavior ();
+		InvokeRepeating ("ReevaluateBehavior", 0, 5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (target != null) {
+			Vector2 direction = target.transform.position - transform.position;
+			Vector2 velocity = direction.normalized * movementSpeed * Time.deltaTime;
+			transform.Translate (velocity);
+		}
 	}
 
 	/// <summary>
@@ -22,5 +37,28 @@ public class Enemy : MonoBehaviour {
 	/// </summary>
 	public void Die () {
 		Destroy (gameObject);
+	}
+
+	/// <summary>
+	/// Reevaluates this enemy's behavior.
+	/// </summary>
+	protected void ReevaluateBehavior () {
+		Friend[] potentialTargets = FindObjectsOfType<Friend> ();
+		float distance = 0;
+		if (potentialTargets.Length > 0) {
+			target = potentialTargets [0];
+			distance = Vector2.Distance (transform.position, target.transform.position);
+			for (int i = 1; i < potentialTargets.Length; i++) {
+				Friend potentialTarget = potentialTargets [i];
+				float potentialDistance = Vector2.Distance (transform.position, potentialTarget.transform.position);
+				if (potentialDistance < distance) {
+					target = potentialTarget;
+					distance = potentialDistance;
+				}
+			}
+		}
+		else {
+			target = null;
+		}
 	}
 }
