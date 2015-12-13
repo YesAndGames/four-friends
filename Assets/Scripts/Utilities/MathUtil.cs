bc2e409a029e4b6260895f0e748766d605e5c6ff
Weslo <wesley.rockholz@gmail.com>
2015-12-12 22:33:59 -0500

    added unity and math utils

---
+++ Assets/Scripts/Utilities/MathUtil.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Basic math utilities used throughout the project.
+/// </summary>
+public static class MathUtil {
+
+	/// <summary>
+	/// Gets the angle between to points in 2D space.
+	/// </summary>
+	/// <returns>The angle between the specified points in radians.</returns>
+	/// <param name="a">The first 2D point in space.</param>
+	/// <param name="b">The second 2D point in space.</param>
+	public static float AngleBetweenPoints (Vector2 a, Vector2 b) {
+		return Mathf.Atan2 (a.y - b.y, a.x - b.x);
+	}
+
+	/// <summary>
+	/// Gets a local radial position given a distance from the origin and an angle in radians.
+	/// </summary>
+	/// <returns>The local position vector from the origin.</returns>
+	/// <param name="distance">Distance from the origin.</param>
+	/// <param name="angle">Angle from the origin in radians.</param>
+	public static Vector2 Vector2FromMagnitudeAndAngle (float distance, float angle) {
+		return Vector2FromMagnitudeAndAngle (distance, angle, Vector2.zero);
+	}
+
+	/// <summary>
+	/// Gets a global radial position given a distance from the origin, an angle in radians, and an origin position.
+	/// </summary>
+	/// <returns>The local position vector from the origin.</returns>
+	/// <param name="distance">Distance from the origin.</param>
+	/// <param name="angle">Angle from the origin in radians.</param>
+	/// <param name="origin">Origin point in 2D space.</param>
+	public static Vector2 Vector2FromMagnitudeAndAngle (float distance, float angle, Vector2 origin) {
+		float x = origin.x + distance * Mathf.Cos (angle);
+		float y = origin.y + distance * Mathf.Sin (angle);
+		return new Vector2 (x, y);
+	}
+
+	/// <summary>
+	/// Gets an angular translation in radians given an arc length travelled and the arc's radius.
+	/// </summary>
+	/// <returns>The angular translation travelled in radians.</returns>
+	/// <param name="length">Length of the arc.</param>
+	/// <param name="radius">Radius of the arc.</param>
+	public static float AngleFromArcLength (float length, float radius) {
+		return length / radius;
+	}
+
+	/// <summary>
+	/// Gets a random number along the normal distribution
+	/// </summary>
+	/// <returns>The from normal distribution.</returns>
+	public static float RandomFromNormalDistribution (float mean = 0, float stdDev = 1, bool positiveOnly = false) {
+
+		// Get the random standard deviation.
+		float randStdNormal = Mathf.Sqrt(-2f * Mathf.Log(Random.value)) * Mathf.Sin(2f * Mathf.PI * Random.value);
+		float randNormal = mean + stdDev * randStdNormal;
+
+		// If positive-only, clamp to positives.
+		if (positiveOnly) {
+			randNormal = Mathf.Abs (randNormal);
+		}
+
+		// Return the random normal number.
+		return randNormal;
+	}
+}
---
+++ Assets/Scripts/Utilities/UnityUtil.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Global library of static tools used on Unity engine data throughout the project.
+/// </summary>
+public static class UnityUtil {
+
+	/// <summary>
+	/// Destroys all the children attached to this game object.
+	/// </summary>
+	/// <param name="gameObject">The specified parent game object.</param>
+	public static void DestroyAllChildren (this GameObject gameObject) {
+		DestroyAllChildren (gameObject.transform);
+	}
+
+	/// <summary>
+	/// Destroys all the children attached to this transform.
+	/// </summary>
+	/// <param name="transform">The specified parent transform.</param>
+	public static void DestroyAllChildren (this Transform transform) {
+		for (int i = 0; i < transform.childCount; i++) {
+			Object.Destroy (transform.GetChild (i));
+		}
+	}
+
+	/// <summary>
+	/// Properly gets the Collider2D under the relevant input.
+	/// </summary>
+	/// <returns>The Collider2D under input.</returns>
+	public static Collider2D GetCollider2DUnderInput () {
+
+		// Get the input position.
+		Vector2 input = GetInputPosition ();
+
+		// Get the collider under the input and return it.
+		return Physics2D.OverlapPoint (input);
+	}
+
+	/// <summary>
+	/// Properly gets the Collider2D under the relevant input.
+	/// </summary>
+	/// <returns>The Collider2D under input.</returns>
+	/// <param name="layer">The string name of the LayerMask to restrict.</param>
+	public static Collider2D GetCollider2DUnderInput (string layer) {
+
+		// Get the input position.
+		Vector2 input = GetInputPosition ();
+
+		// Get the collider under the input and return it.
+		return Physics2D.OverlapPoint (input, 1 << LayerMask.NameToLayer (layer));
+	}
+
+	/// <summary>
+	/// Gets the platform-relevant input position.
+	/// </summary>
+	/// <returns>The input position.</returns>
+	public static Vector2 GetInputPosition () {
+
+		// Default to mouse position.
+		Vector2 input = Input.mousePosition;
+
+		// Use input to determine world position in 2D space.
+		Vector3 wp = Camera.main.ScreenToWorldPoint (input);
+		Vector2 inputPosition = new Vector2 (wp.x, wp.y);
+
+		// Return the calculated position.
+		return inputPosition;
+	}
+}
