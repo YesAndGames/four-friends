96bfb5910d82db8d136b85999ca5879b22c61b13
Weslo <wesley.rockholz@gmail.com>
2015-12-13 00:17:38 -0500

    implement button wheel

---
+++ Assets/Scripts/UserInterface/FriendWheel.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Controls the FriendWheel user interface.
+/// </summary>
+public class FriendWheel : MonoBehaviour {
+
+	/// <summary>
+	/// References to the friend buttons.
+	/// </summary>
+	[SerializeField] private RectTransform northButton, eastButton, southButton, westButton;
+
+	/// <summary>
+	/// The color that represents each color direction.
+	/// </summary>
+	[SerializeField] private Color northColor, eastColor, southColor, westColor;
+
+	/// <summary>
+	/// Time it takes to rotate the buttons.
+	/// </summary>
+	[SerializeField] private float rotateTime = 0.1f;
+
+	/// <summary>
+	/// Distance buttons spread out visually.
+	/// </summary>
+	[SerializeField] private int buttonSpread = 40;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start () {
+
+		// Assign initial colors.
+		northButton.GetComponent<Image> ().color = northColor;
+		eastButton.GetComponent<Image> ().color = eastColor;
+		southButton.GetComponent<Image> ().color = southColor;
+		westButton.GetComponent<Image> ().color = westColor;
+
+		// Initialize button positioning.
+		northButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.North, buttonSpread), 0);
+		eastButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.East, buttonSpread), 0);
+		southButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.South, buttonSpread), 0);
+		westButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.West, buttonSpread), 0);
+
+		// Hook controls.
+		Controls.RotateLeft.AddListener (OnRotateLeft);
+		Controls.RotateRight.AddListener (OnRotateRight);
+	}
+
+	/// <summary>
+	/// Rotate the party to the left.
+	/// </summary>
+	private void OnRotateLeft () {
+
+		// North -> West
+		northButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.West, buttonSpread), rotateTime);
+		northButton.GetComponent<Image> ().color = westColor;
+
+		// West -> South
+		westButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.South, buttonSpread), rotateTime);
+		westButton.GetComponent<Image> ().color = southColor;
+
+		// South -> East
+		southButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.East, buttonSpread), rotateTime);
+		southButton.GetComponent<Image> ().color = eastColor;
+
+		// East -> North
+		eastButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.North, buttonSpread), rotateTime);
+		eastButton.GetComponent<Image> ().color = northColor;
+
+		// Move references.
+		RectTransform cachedButton = northButton;
+		northButton = eastButton;
+		eastButton = southButton;
+		southButton = westButton;
+		westButton = cachedButton;
+	}
+
+	/// <summary>
+	/// Rotate the party to the right.
+	/// </summary>
+	private void OnRotateRight () {
+
+		// North -> East
+		northButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.East, buttonSpread), rotateTime);
+		northButton.GetComponent<Image> ().color = eastColor;
+
+		// West -> North
+		westButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.North, buttonSpread), rotateTime);
+		westButton.GetComponent<Image> ().color = northColor;
+
+		// South -> West
+		southButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.West, buttonSpread), rotateTime);
+		southButton.GetComponent<Image> ().color = westColor;
+
+		// East -> South
+		eastButton.GetComponent<RectTransformLerper> ().MoveTo (
+			DirectionUtil.GetDirectionVector (Direction.South, buttonSpread), rotateTime);
+		eastButton.GetComponent<Image> ().color = southColor;
+
+		// Move references.
+		RectTransform cachedButton = northButton;
+		northButton = westButton;
+		westButton = southButton;
+		southButton = eastButton;
+		eastButton = cachedButton;
+	}
+}
---
+++ Assets/Scripts/Utilities/RectTransformLerper.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Abstracts and manages interpolated rect transform movement.
+/// </summary>
+public class RectTransformLerper : MonoBehaviour {
+
+	/// <summary>
+	/// Positions used for interpolation.
+	/// </summary>
+	private Vector2 initialPosition, targetPosition;
+
+	/// <summary>
+	/// Interpolation trackers.
+	/// </summary>
+	private float lerpTime = 0, t = 0;
+
+	/// <summary>
+	/// Flag for still interpolating for efficiency.
+	/// </summary>
+	private bool interpolating = false;
+
+	/// <summary>
+	/// Update this component.
+	/// </summary>
+	void Update () {
+		if (interpolating) {
+			t += Time.deltaTime;
+
+			if (t >= lerpTime) {
+				t = lerpTime;
+				interpolating = false;
+				GetComponent<RectTransform> ().anchoredPosition = targetPosition;
+			}
+			else {
+				GetComponent<RectTransform> ().anchoredPosition = Vector2.Lerp (initialPosition, targetPosition, t / lerpTime);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Move to the specified position in the specified time.
+	/// </summary>
+	/// <param name="position">Position.</param>
+	/// <param name="time">Time.</param>
+	public void MoveTo (Vector2 position, float time) {
+		initialPosition = GetComponent<RectTransform> ().anchoredPosition;
+		targetPosition = position;
+		lerpTime = time;
+		t = 0;
+		interpolating = true;
+	}
+}
