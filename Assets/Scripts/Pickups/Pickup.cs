d27794d79ba8fb9e7e835718df0c193eab8eaf7f
Weslo <wesley.rockholz@gmail.com>
2015-12-14 17:35:53 -0500

    health potion sounds

--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -6,6 +6,11 @@
 public class Pickup : MonoBehaviour {
 
 	/// <summary>
+	/// Play this clip on pickup.
+	/// </summary>
+	[SerializeField] private AudioClip pickupSound;
+
+	/// <summary>
 	/// Called when this trigger enters another trigger.
 	/// </summary>
 	void OnTriggerEnter2D (Collider2D other) {
@@ -17,6 +22,14 @@ public class Pickup : MonoBehaviour {
 	/// </summary>
 	/// <param name="other">Thing that picked this thing up.</param>
 	protected virtual void OnPickup (Collider2D other) {
+
+		// Play the sound on the collider.
+		if (pickupSound != null) {
+			AudioSource source = other.GetComponentInChildren<AudioSource> ();
+			if (source != null) {
+				source.PlayOneShot (pickupSound);
+			}
+		}
 
 		// Self-destruct.
 		Destroy (gameObject);
