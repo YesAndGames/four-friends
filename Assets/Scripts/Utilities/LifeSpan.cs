f26867ae4be4675722a1a747c80f4b6f53fcf66d
Weslo <wesley.rockholz@gmail.com>
2015-12-14 12:23:34 -0500

    implement life spans on zones

---
+++ Assets/Scripts/Utilities/LifeSpan.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Component that destroys its own GameObject after a given amount of time.
+/// </summary>
+public class LifeSpan : MonoBehaviour {
+
+	/// <summary>
+	/// The life span of this object.
+	/// </summary>
+	[SerializeField] private float lifeSpan = 10f;
+
+	/// <summary>
+	/// Time passed.
+	/// </summary>
+	private float t = 0;
+
+	/// <summary>
+	/// Initialize this component.
+	/// </summary>
+	void Start () {
+		t = 0;
+	}
+
+	/// <summary>
+	/// Update this component.
+	/// </summary>
+	void Update () {
+		t += Time.deltaTime;
+		if (t >= lifeSpan) {
+			DestroySelf ();
+		}
+	}
+
+	/// <summary>
+	/// Destroy this game object.
+	/// </summary>
+	private void DestroySelf () {
+		Destroy (gameObject);
+	}
+}
