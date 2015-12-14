308d43d3ab6db99102e68fba9fb680577325716d
Weslo <wesley.rockholz@gmail.com>
2015-12-13 20:58:17 -0500

    successfully clean up loose projectiles

--- Assets/Scripts/StateManagement/GameplayScreen.cs
+++ Assets/Scripts/StateManagement/GameplayScreen.cs
@@ -16,6 +16,18 @@ public class GameplayScreen : IGameState {
 	}
 
 	/// <summary>
+	/// Clean up loose projectiles in scene.
+	/// </summary>
+	void OnDestroy () {
+		base.OnExitState ();
+
+		Projectile[] looseProjectiles = FindObjectsOfType<Projectile> ();
+		for (int i = 0; i < looseProjectiles.Length; i++) {
+			Destroy (looseProjectiles [i].gameObject);
+		}
+	}
+
+	/// <summary>
 	/// Call to invoke game over.
 	/// </summary>
 	private void OnGameOver () {
